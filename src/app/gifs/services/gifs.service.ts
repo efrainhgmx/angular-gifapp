b94b0ffb624144a4ecea618a515d287b305227be
Efrain Hernandez <efrainhg2050@gmail.com>
2022-08-01 17:44:44 -0500

    Interface for api

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGIFResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class GifsService {
   private apiKey: string = 'qBDboQvMFViShZQ71t43CvOQG6kW77Sa';
   private _historial: string[] = [];
 
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
 
   get historial(): string[] {
@@ -24,8 +25,8 @@ export class GifsService {
       this._historial = this._historial.slice(0, 9);
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=qBDboQvMFViShZQ71t43CvOQG6kW77Sa&q=${query}&limit=10`)
-    .subscribe((resp:any) => {
+    this.http.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=qBDboQvMFViShZQ71t43CvOQG6kW77Sa&q=${query}&limit=10`)
+    .subscribe((resp) => {
       this.resultados = resp.data;
       console.log(this.resultados);
     })
