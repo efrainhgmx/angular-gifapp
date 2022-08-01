f1b973c5089c7fda00ee813952e7949dfa444b5e
Efrain Hernandez <efrainhg2050@gmail.com>
2022-08-01 18:31:38 -0500

    parms http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { query } from 'express';
 import { Gif, SearchGIFResponse } from '../interface/gifs.interface';
 
 @Injectable({
@@ -8,6 +9,7 @@ import { Gif, SearchGIFResponse } from '../interface/gifs.interface';
 export class GifsService {
   private apiKey: string = 'qBDboQvMFViShZQ71t43CvOQG6kW77Sa';
   private _historial: string[] = [];
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs'
 
   public resultados: Gif[] = [];
 
@@ -16,7 +18,12 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient){}
+  //Solo se ejecuta una vez que es llamado
+  constructor(private http: HttpClient){
+    this._historial = JSON.parse(localStorage.getItem('historial')!) ?? [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) ?? [];
+
+  }
 
   buscarGifs( query: string) {
     query = query.trim().toLowerCase();
@@ -25,9 +32,18 @@ export class GifsService {
       this._historial = this._historial.slice(0, 9);
     }
 
-    this.http.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=qBDboQvMFViShZQ71t43CvOQG6kW77Sa&q=${query}&limit=10`)
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', 10)
+    .set('q', query);
+
+    localStorage.setItem('historial', JSON.stringify(this._historial));
+    
+    
+    this.http.get<SearchGIFResponse>(`${this.servicioUrl}/search`, { params })
     .subscribe((resp) => {
       this.resultados = resp.data;
+      localStorage.setItem('resultados', JSON.stringify(this.resultados))
       console.log(this.resultados);
     })
 
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -13,4 +13,7 @@ export class SidebarComponent  {
 
   constructor(private gifsService: GifsService){}
 
+  buscar(query: string) {
+    this.gifsService.buscarGifs(query);
+  }
 }
